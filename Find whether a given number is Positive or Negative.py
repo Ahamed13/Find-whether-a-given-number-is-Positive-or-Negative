886eb0d6cbdfe8fcfc34e48b23551bd3c472b807
Ahamed Sheak Ansari S <80703598+Ahamed13@users.noreply.github.com>
2021-10-07 13:01:50 +0530

    Add files via upload

---
+++ Find whether a given number is Positive or Negative.py
@@ -0,0 +1,21 @@
+#Find whether a given number is Positive or Negative
+
+n = int(input('Enter the Number: '))
+
+if n < 0:
+
+    print('The Number {} is Negative Number'.format(n))
+
+else:
+
+    print('The Number {} is Positive Number'.format(n))
+
+
+
+
+
+
+
+
+
+
